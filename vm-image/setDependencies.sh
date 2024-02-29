69a801ba7953eeffbaeae290dbbe10b91de0a837
Payal Khatri <khatri.p@northeastern.edu>
2024-02-29 08:41:51 -0500

    Removing commented code and setting image back to build at merge

--- vm-image/setDependencies.sh
+++ vm-image/setDependencies.sh
@@ -9,22 +9,6 @@ sudo chown csye6225:csye6225 webapp -R
 
 cd /home/Cloud/webapp/ || exit
 
-# env_values=$(cat <<END
-# PORT=$PORT
-# DB_NAME=$DB_NAME
-# DB_USER=$DB_USER
-# DB_PASSWORD=$DB_PASSWORD
-# HOST=$HOST
-# DIALECT=$DIALECT
-# END
-# )
-
-# echo "$env_values" | sudo tee .env >/dev/null
-
-# sudo chown csye6225:csye6225 .env 
-
-# echo ".env file created"
-
 sudo npm install 
 
 echo "NPM packages installed successfully."
