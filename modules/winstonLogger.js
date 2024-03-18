f3575b6a0de48c986569ef4026c2ebc97a459ffa
Payal Khatri <khatri.p@northeastern.edu>
2024-03-18 18:42:26 -0400

    Adding NODE_ENV env variable to check if it's dev or prod

--- modules/winstonLogger.js
+++ modules/winstonLogger.js
@@ -1,16 +1,21 @@
-import winston from 'winston';
+import winston from "winston";
 
 // Create a Winston logger instance
 const logger = winston.createLogger({
-  transports: [
-    new winston.transports.File({filename: '/var/log/webapp_logs/webappLog.log'})
-  ],
+  transports:
+    process.env.NODE_ENV == "PROD"
+      ? [
+          new winston.transports.File({
+            filename: "/var/log/webapp_logs/webappLog.log",
+          }),
+        ]
+      : [new winston.transports.Console()],
   format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.printf(({ timestamp, level, message }) => {
       return `${timestamp} ${level}: ${message}`;
     })
-  )
+  ),
 });
 
 export default logger;
