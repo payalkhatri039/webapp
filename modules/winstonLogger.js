189a1bbb19542ae0e8b8250e052b653ce8564355
Payal Khatri <khatri.p@northeastern.edu>
2024-03-18 15:30:24 -0400

    changing destination of log file to /var/log

--- modules/winstonLogger.js
+++ modules/winstonLogger.js
@@ -3,7 +3,7 @@ import winston from 'winston';
 // Create a Winston logger instance
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.File({filename: '/var/logs/webapp_logs/myapp.log'})
+    new winston.transports.File({filename: '/var/log/webapp_log/webappLog.log'})
   ],
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
