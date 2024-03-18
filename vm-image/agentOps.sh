bd0173aa09911389d4fb8eb48e59fed5ee044bbf
Payal Khatri <khatri.p@northeastern.edu>
2024-03-18 14:52:41 -0400

    Going into the user directory to install agent ops

--- vm-image/agentOps.sh
+++ vm-image/agentOps.sh
@@ -6,11 +6,9 @@ cd /var/logs/ || exit
 
 sudo chown csye6225:csye6225 webapp_logs -R 
 
-cd /home || exit
+cd && curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+cd  && sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/
 
