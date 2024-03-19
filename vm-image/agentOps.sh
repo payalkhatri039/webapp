0e6a7f70cace289ded6bdc8364e0276ad12b61a1
Payal Khatri <khatri.p@northeastern.edu>
2024-03-18 23:52:32 -0400

    Correcting permission for config.yaml file

--- vm-image/agentOps.sh
+++ vm-image/agentOps.sh
@@ -14,9 +14,9 @@ cd  && sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/
 
-cd /etc/google-cloud-ops-agent/ && sudo chown csye6225:csye6225 config.yaml
+sudo chmod 775 /etc/google-cloud-ops-agent/config.yaml
 
-cd /etc/google-cloud-ops-agent/ && sudo chmod 775 csye6225:csye6225 config.yaml
+cd /etc/google-cloud-ops-agent/ && sudo chown csye6225:csye6225 config.yaml
 
 sudo systemctl restart google-cloud-ops-agent
 
