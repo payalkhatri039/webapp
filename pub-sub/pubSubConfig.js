96183e16202be45724e64cf6c9c44b1a1a969bd6
Payal Khatri <khatri.p@northeastern.edu>
2024-04-10 02:30:06 -0400

    Making the version in url variable and testing it with v2

--- pub-sub/pubSubConfig.js
+++ pub-sub/pubSubConfig.js
@@ -1,20 +1,20 @@
 import { PubSub } from "@google-cloud/pubsub";
 import logger from "../modules/winstonLogger.js";
+import { version } from "winston";
 // Create an instance of PubSub with the provided service account key
 const pubSubClient = new PubSub({
   projectId: "csye6225-a03",
 });
- 
-
-
 
 export const publishMessage = async (topicName, payload) => {
-  const dataBuffer = Buffer.from(JSON.stringify(payload));
+  const userData = { ...payload, version: "v2" };
+
+  const dataBuffer = Buffer.from(JSON.stringify(userData));
   try {
     const messageId = await pubSubClient
       .topic(topicName)
       .publishMessage({ data: dataBuffer });
-      logger.info(`Message ${messageId} published.`);
+    logger.info(`Message ${messageId} published.`);
     return messageId;
   } catch (error) {
     logger.error(`Received error while publishing: ${error.message}`);
